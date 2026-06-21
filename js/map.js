05a6a2eb1ee3e636e0032d490a13a0d6684422fb
shahmuradovk <shahmuradovk@users.noreply.github.com>
2026-06-21 14:39:55 +0400

    Fix 404: switch to CartoDB light_all tiles

--- js/map.js
+++ js/map.js
@@ -66,7 +66,7 @@ const MotoMap = {
     });
 
     L.tileLayer(
-      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
       {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
